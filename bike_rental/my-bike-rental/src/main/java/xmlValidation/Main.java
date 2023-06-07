134fca9dc7070cb1c53e4322d753c9c8ca933a69
Julia J-B <4juliko@gmail.com>
2023-06-07 13:19:22 -0700

    log xml removed

--- bike_rental/my-bike-rental/src/main/java/xmlValidation/Main.java
+++ bike_rental/my-bike-rental/src/main/java/xmlValidation/Main.java
@@ -15,8 +15,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
 	public static void main (String[] args) {

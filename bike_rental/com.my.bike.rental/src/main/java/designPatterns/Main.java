30b212996bc16b6ce7ba9498f2b6044940bd3de4
Julia J-B <4juliko@gmail.com>
2023-06-26 21:04:58 -0700

    Design patterns added

--- bike_rental/com.my.bike.rental/src/main/java/designPatterns/Main.java
+++ bike_rental/com.my.bike.rental/src/main/java/designPatterns/Main.java
@@ -4,7 +4,7 @@ import designPatterns.serviceInterface.IBikeService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class PatternRunner {
+public class Main {
 	public static void main(String[] args) {
 		Logger logger = LogManager.getLogger();
 
